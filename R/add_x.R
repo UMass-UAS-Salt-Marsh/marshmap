0637fe9329dfe28f1b1926242b68e0f5c05057e2
bwcompton <bcompton@umass.edu>
2025-06-26 17:47:05 +0000

    Finally get addx in check_files working, I think

--- R/add_x.R
+++ R/add_x.R
@@ -3,13 +3,16 @@
 #' This prevents downstream problems
 #' 
 #' @param x Vector of filenames
+#' @param ignore If TRUE, don't do anything
 #' @returns Filenames with 'x' prepended for those starting with a digit
 #' @keywords internal
 
 
-add_x <- function(x) {
+add_x <- function(x, ignore = FALSE) {
    
    
+   if(ignore)
+      return(x)
    i <- grep('^\\d', x)
    x[i] <- paste0('x', x[i])
    x
--- R/check_files.R
+++ R/check_files.R
@@ -1,36 +1,35 @@
-   #' Check that each source file exists on the result directory and is up to date
-   #' 
-   #' @param files Vector of files to check
-   #' @param gd Source Drive info (optional), named list of 
-   #'    - `dir`            Google directory info, from [get_dir]
-   #'    - `sourcedrive`    which source drive (`local`, `google`, or `sftp`)
-   #'    - `sftp`           `list(url, user)`
-   #' @param sourcedir Origin directory of files
-   #' @param resultdir Target directory of files - see if origin files are here and up to date
-   #' @param addx If TRUE, prepends 'x' to files starting with a digit
-   #' @returns A vector corresponding to files, TRUE for those that are up to date
-   #' @importFrom googledrive drive_reveal
-   #' @export
+#' Check that each source file exists on the result directory and is up to date
+#' 
+#' @param files Vector of files to check
+#' @param gd Source Drive info (optional), named list of 
+#'    - `dir`            Google directory info, from [get_dir]
+#'    - `sourcedrive`    which source drive (`local`, `google`, or `sftp`)
+#'    - `sftp`           `list(url, user)`
+#' @param sourcedir Origin directory of files
+#' @param resultdir Target directory of files - see if origin files are here and up to date
+#' @param addx If TRUE, prepends 'x' to files starting with a digit
+#' @returns A vector corresponding to files, TRUE for those that are up to date
+#' @importFrom googledrive drive_reveal
+#' @export
+
+
+check_files <- function(files, gd, sourcedir, resultdir, addx = FALSE) {
    
-      
-      'check_files' <- function(files, gd, sourcedir, resultdir, addx = FALSE) {
-         
-      
+   
    z <- rep(FALSE, length(files))
    
-   for(i in 1:length(files))                                                              # for each file,
-      f <- g <- file.path(resultdir, file.path(basename(files[i])))                       #    file name
-      if(addx)                                                                           #       if addx, prepend 'x' if necessary
-         g <- add_x(g)
-      if(file.exists(g)) {                                                                #    if the file exists in the results directory,
+   for(i in seq_along(files)) {                                                           # for each file,
+      f <- file.path(resultdir, file.path(add_x(basename(files[i]), ignore = !addx)))     #    local file path and name
+      if(file.exists(f)) {                                                                #    if the file exists in the results directory,
          sdate <- switch(gd$sourcedrive,                                                  #       get last modified date on source drive
                          'local' = file.mtime(f),
                          'google' = drive_reveal(gd$dir[gd$dir$name == files[i], ],
                                                  what = 'modified_time')$modified_time,
-                         'sftp' = gd$dir$date[basename(gd$dir$name) == basename(f)]
+                         'sftp' = gd$dir$date[basename(gd$dir$name) == basename(files[i])]
          )
          rdate <- file.mtime(f)                                                           #   date on result drive
          z[i] <- rdate >= sdate                                                           #   TRUE if it's present and up to date
       }
+   }
    z
 }
