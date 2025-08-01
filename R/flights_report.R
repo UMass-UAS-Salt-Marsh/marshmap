884b041e2058a7ad2b6baf6106444fb34836d0f8
bwcompton <bcompton@umass.edu>
2025-08-01 21:21:28 +0000

    Fill out flights_report: add summary (partly done), dups, and all orthos

--- R/flights_report.R
+++ R/flights_report.R
@@ -1,47 +1,87 @@
 #' Produce a flights report for one or more sites
 #'
-#' The site report is a tab-delimited text file listing images flagged for repair.
+#' The site report is seroes of tab-delimited text files:
+#' 
+#'   1. Summary of orthos for each site
+#'   2. List of orthos flagged for repair
+#'   3. List of duplicated portable names (* marks selected names)
+#    4. List of all orthos
 #'    
-#' Files are written to `reports/flagged_for_repair.txt`.
-#'    
-#' @param site One or more site names, using 3 letter abbreviation. Use `all` to process all sites. 
+#' Files are written to the `reports/` directory.
+#'
 #' @importFrom grDevices pdf
 #' @importFrom gridExtra grid.table
 #' @export
 
-# I might add the following at some point if there's need:
-#    1. Flights summary page
-#    2. List of duplicated portable names (* marks selected names)
-#    3. List of all orthos
 
-flights_report <- function(site = 'all') {
+flights_report <- function() {
    
    
-   sites <- get_sites(site)
-   z <- NULL
-   for(site in sites) {                                                                # for each site,
-      db <- get_flights_db(site, noerror = TRUE)                                       #    get the flights database
+   sites <- get_sites('all')
+   z_summary <- z_repair <- z_dups <- z_all <- NULL
+   for(site in sites) {                                                                   # for each site,
+      db <- get_flights_db(site, noerror = TRUE)                                          #    get the flights database
+      
       if(!is.null(db)) {
-         db <- db[db$repair == TRUE, ]
-         if(nrow(db) >= 1) {
-            if(is.null(z))
-               z <- data.frame(site = site, file = db$name, score = db$score, comment = db$comment)
+         db$site <- site
+         
+         x <- paste0('\n\nSite: ', toupper(site), '\n', nrow(db), ' images, ', 
+                     sum(db$score != 0), ' scored (', round(sum((db$score != 0) / nrow(db)) * 100, 0), '%)')
+         z_summary <- c(z_summary, x)
+         
+         
+         repair <- db[db$repair == TRUE, ]                                                # 2. list of files flagged for repair
+         if(nrow(repair) >= 1) {
+            repair <- repair[, c('site', 'name', 'score', 'comment')]
+            if(is.null(z_repair))
+               z_repair <- repair
             else
-               z <- rbind(z, data.frame(site = site, file = db$name, score = db$score, comment = db$comment))
+               z_repair <- rbind(z_repair, repair)
          }
       }
       
-      # dups <- db[db$dups > 1, ]                                                      # list duplicated portable names
-      # dups$pick <- ''
-      # dups$pick[sapply(unique(dups$portable), function(x) pick(x, dups))] <- '*'
-      # dups <- dups[order(dups$portable, dups$pick != '*'), ]
-      # dups <- dups[, c('portable', 'name', 'pick', 'dups', 'season', 'score')]
-      # row.names(dups) <- NULL                                                          # reset row numbers
+      dups <- db[db$dups > 1, ]                                                           # 3. list duplicated portable names
+      if(nrow(dups) >= 1) {
+         dups$pick <- ''
+         dups$pick[sapply(unique(dups$portable), function(x) pick(x, dups))] <- '*'
+         dups <- dups[order(dups$portable, dups$pick != '*'), ]
+         dups <- dups[, c('site', 'portable', 'name', 'pick', 'dups', 'season', 'score')]
+         row.names(dups) <- NULL                                                             # reset row numbers
+         
+         if(is.null(z_dups))
+            z_dups <- dups
+         else
+            z_dups <- rbind(z_dups, dups)
+      }
+      
+      all <- db[order(db$type, db$sensor, db$derive, db$window, db$tide, db$tidemod, 
+                      db$season, db$year, db$score, db$repair), ]                         # 4. list all orthos
+      all <- db[, c('site', 'portable', 'name', 'type', 'sensor', 'derive', 'window', 'tide', 'tidemod',
+                    'season', 'year', 'score', 'repair')]
+      
+      if(is.null(z_all))
+         z_all <- all
+      else
+         z_all <- rbind(z_all, all)
    }
+   
    
    if(!dir.exists(the$reportsdir))
       dir.create(the$reportsdir, recursive = TRUE)
+   
+   
+   f <- file.path(the$reportsdir, 'summary.txt')
+   write.table(z_summary, f, sep = '\t', quote = FALSE, row.names = FALSE, na = '')
+   
+   f <- file.path(the$reportsdir, 'duplicates.txt')
+   write.table(z_dups, f, sep = '\t', quote = FALSE, row.names = FALSE, na = '')
+   
    f <- file.path(the$reportsdir, 'flagged_for_repair.txt')
-   write.table(z, f, sep = '\t', quote = FALSE, row.names = FALSE, na = '')
-   cat(nrow(z), ' files marked for repair written to ', f, '\n', sep = '')
+   write.table(z_repair, f, sep = '\t', quote = FALSE, row.names = FALSE, na = '')
+   
+   f <- file.path(the$reportsdir, 'all_orthos.txt')
+   write.table(z_all, f, sep = '\t', quote = FALSE, row.names = FALSE, na = '')
+   
+   
+   cat('Reports written to ', the$reportsdir, '\n', sep = '')
 }
