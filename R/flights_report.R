cc811fae0114b9a0b459e20d618a012e4317f134
bwcompton <bcompton@umass.edu>
2025-08-05 18:06:10 +0000

    Revise doc for flights_report

--- R/flights_report.R
+++ R/flights_report.R
@@ -1,11 +1,18 @@
 #' Produce a flights report for one or more sites
 #'
-#' The site report is series of tab-delimited text files:
+#' The site report is series text files. 
 #' 
-#'   1. Summary of orthos for each site
-#'   2. List of orthos flagged for repair
-#'   3. List of duplicated portable names (* marks selected names)
-#    4. List of all orthos
+#'   1. Summary of orthos for each site, with stats and frequency tables
+#'      for each site. Unformatted text file, suitable for viewing in a text
+#'      editor.
+#'   2. List of orthos flagged for repair. Tab-delimited text file with columns 
+#'      `site`, `name`, `score`, and `comment`. Best viewed in Excel or read into R.
+#'   3. List of duplicated portable names. Tab-delimited text file with columns
+#'      `site`, `portable`, `name`, `pick` (`*` for selected images), `dups`, 
+#'      `season`, and `score`. Best viewed in Excel or read into R.
+#'   4. List of all orthos. Tab-delimited text file with columns `site`, `portable`, 
+#'      `name`, `type`, `sensor`, `derive`, `window`, `tide`, `tidemod`, `season`, 
+#'      `year`, `score`, and `repair`.  Best viewed in Excel or read into R.
 #'    
 #' Files are written to the `reports/` directory.
 #'
