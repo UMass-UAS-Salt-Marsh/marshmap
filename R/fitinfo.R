f6d0b0a21d5539d58b895033ff7231366e0f05bb
bwcompton <bcompton@umass.edu>
2025-08-21 19:12:25 -0400

    Enough of fitinfo to make pkgdown happy, maybe

--- R/fitinfo.R
+++ R/fitinfo.R
@@ -1,6 +1,13 @@
-
+#' Give info on models that have been built with `fit`
+#' 
+#' @param rows Selected rows in the fits database
+#' @param cols Selected columns to display
+#' @export
 
 
 fitinfo <- function() {
+   
+   
+   print('TBD')
    
 }
