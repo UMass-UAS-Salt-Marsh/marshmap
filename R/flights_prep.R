04c7414cb24b9e07327b439bb88d02a436b90cbf
bwcompton <bcompton@umass.edu>
2025-09-25 22:21:58 +0000

    Finally get it right

--- R/flights_prep.R
+++ R/flights_prep.R
@@ -21,15 +21,15 @@ flights_prep <- function(site) {
    
    message('\nRunning flights_prep...')
    
-   db <- build_flights_db(site)                                         # update flights database
+   db <- build_flights_db(site)                                               # update flights database
    
    dir <- resolve_dir(the$flightsdir, site)
-   fd <- file.mtime(file.path(dir, db$db$name))                         # file dates on disk
+   fd <- file.mtime(file.path(dir, db$db$name))                               # file dates on disk
    update <- is.na(db$db$missing_filestamp) |
-      db$db$missing_filestamp < fd                                      # files that are new or outdated,
+      db$db$missing_filestamp < fd                                            # files that are new or outdated,
    
    
-   if(any(update)) {                                                    # if any files have been updated, we have to read them all
+   if(any(update)) {                                                          # if any files have been updated, we have to read them all
       
       message('Updating missing value counts for ', 
               sum(update), ' orthos and all_miss raster...')
@@ -39,27 +39,29 @@ flights_prep <- function(site) {
       fp <- basename(sites$footprint[sites$site == site])
       footprint <- vect(file.path(resolve_dir(the$shapefilesdir, site), fp))
       
-      for(i in seq_len(nrow(db$db))) {                                  #    for each ortho,
+      for(i in seq_len(nrow(db$db))) {                                        #    for each ortho,
          
          print(i)
          
-         x <- rast(file.path(dir, db$db$name[i]))[[1]]                  #       read raster (* we assume NAs are shared across all bands)
-         if(i == 1) {                                                   #       if first one,
-            fp <- global(mask(setValues(x, 1), footprint), 
-                         'sum', na.rm = TRUE)                           #          count of cells in footprint
-            all_miss <- setValues(x, 0)                                 #          create missing raster
+         x <- rast(file.path(dir, db$db$name[i]))[[1]]                        #       read raster (* we assume NAs are shared across all bands)
+         if(i == 1) {                                                         #       if first one,
+            fp <- mask(setValues(x, 1), footprint)                            #          footprint (1 inside, NA in border)
+            inside <- as.numeric(global(fp, 'sum', na.rm = TRUE))             #          count of cells in footprint
+            border <- as.numeric(global(is.na(fp), 'sum'))                    #          and of cells in border
+            all_miss <- setValues(x, 0)                                       #          create missing raster
          }
          
-         all_miss <- all_miss + is.na(x)                                #       and count missing
+         all_miss <- all_miss + is.na(x)                                      #       and count missing
          
-         if(update[i]) {                                                #       if this file needs updating,
+         if(update[i]) {                                                      #       if this file needs updating,
             db$db$pct_missing[i] <- 
-               (global(!is.na(x), 'sum')) / fp * 100                    #          get percent missing
-            db$db$bands <- nlyr(rast(file.path(dir, db$db$name[i])))    #          get number of bands
+               round(((as.numeric(global(is.na(x), 'sum')) - border) / 
+                         inside * 100), 2)                                    #          get percent missing
+            db$db$bands[i] <- nlyr(rast(file.path(dir, db$db$name[i])))       #          get number of bands
          }
       }
       
-      db$db$missing_filestamp[update] <- now()                          # update missing_filestamp
+      db$db$missing_filestamp[update] <- now()                                # update missing_filestamp
       
       rd <- resolve_dir(the$reportsdir, site)
       all_miss <- mask(all_miss, footprint)
@@ -67,12 +69,12 @@ flights_prep <- function(site) {
       
       writeRaster(all_miss, paste0(am, '000.tif'), overwrite = TRUE,
                   datatype = 'INT2S', 
-                  NAflag = assessType('INT2S')$noDataValue)             # save all_miss, count of missing values in all rasters
+                  NAflag = assessType('INT2S')$noDataValue)                   # save all_miss, count of missing values in all rasters
       
       makeNiceTif(source = paste0(am, '000.tif'), 
                   destination = paste0(am, '.tif'), 
                   overwrite = TRUE,                               
-                  overviewResample = 'average', stats = TRUE)           # make a nice TIFF
+                  overviewResample = 'average', stats = TRUE)                 # make a nice TIFF
       
       unlink(paste0(am, '000.*'))
    }
