dfe9d4d32bbd6601dc2bc768c94441e95ea9034b
bwcompton <bcompton@umass.edu>
2025-09-26 20:11:02 +0000

    Modify screen.plot to split making and rendering images; as expected, making is kinda slow and rendering takes almost no time

--- R/screen_plot.R
+++ R/screen_plot.R
@@ -21,6 +21,12 @@ screen_plot <- function(extent, sensor, bands, input, output, session) {
                   inset2 = session$userData$inset2
    )
    
+   cachedir <- file.path(session$userData$dir, 'cache')
+   if(!dir.exists(cachedir))
+      dir.create(cachedir, recursive = TRUE
+      )
+   cache <- file.path(cachedir, paste0(file_path_sans_ext(session$userData$db$name[session$userData$index]), '_', extent, '.png'))
+   
    
    if(length(grep('Mica', sensor)) != 0)                       # deal with crazy backwards RGB in Mica images
       rgb <- c(3, 2, 1)
@@ -28,18 +34,51 @@ screen_plot <- function(extent, sensor, bands, input, output, session) {
       rgb <- c(1, 2, 3)
    
    
-   renderPlot({
-      if(bands == 1) {
-         plot(data, col = map.pal('viridis'), breaks = 10, breakby = 'cases', legend = FALSE, axes = FALSE, mar = 0.5)
-         
-         # Bug in terra::.get_breaks (https://github.com/rspatial/terra/blob/2a001f940ea658c730c2135be74d41050a6ad13d/R/plot_helper.R#L165)
-         # when using breaks > ~20 for image OTH_Aug_CHM_CSF2012_Thin25cm_TriNN8cm.tif. After breaks <- unique(breaks), they need to reduce
-         # n if necessary or you get `Error in if ((breaks[n]%%1) != 0) { : missing value where TRUE/FALSE needed`
-
-      }
-      else
-         plotRGB(data, rgb[1], rgb[2], rgb[3], stretch = 'lin', mar = 0.5)
-      if(extent == 'full')
-         lines(session$userData$footprint, lwd = 3, col = 'red')
-   })
+   bands <- nlyr(data)  # ************************ value of bands passed in is sometimes wrong!!! **********************
+   
+   
+   old <- TRUE
+   if(old)
+      renderPlot({
+         if(bands == 1) {
+            a <- Sys.time()
+            plot(data, col = map.pal('viridis'), breaks = 10, breakby = 'cases', legend = FALSE, axes = FALSE, mar = 0.5)
+            cat('Making & plotting single-band image takes ', Sys.time() - a, ' secs\n', sep = '')
+            
+            # Bug in terra::.get_breaks (https://github.com/rspatial/terra/blob/2a001f940ea658c730c2135be74d41050a6ad13d/R/plot_helper.R#L165)
+            # when using breaks > ~20 for image OTH_Aug_CHM_CSF2012_Thin25cm_TriNN8cm.tif. After breaks <- unique(breaks), they need to reduce
+            # n if necessary or you get `Error in if ((breaks[n]%%1) != 0) { : missing value where TRUE/FALSE needed`
+         }
+         else {
+            a <- Sys.time()
+            plotRGB(data, rgb[1], rgb[2], rgb[3], stretch = 'lin', mar = 0.5)
+            cat('Making & plotting three-band image takes ', Sys.time() - a, ' secs\n', sep = '')
+            
+            if(extent == 'full')
+               lines(session$userData$footprint, lwd = 3, col = 'red')
+         }
+      })
+   else {
+         a <- Sys.time()
+         png(cache)
+         if(bands == 1)
+            plot(data, col = map.pal('viridis'), breaks = 10, breakby = 'cases', legend = FALSE, axes = FALSE, mar = 0.5)
+         else
+            plotRGB(data, rgb[1], rgb[2], rgb[3], stretch = 'lin', mar = 0.5)
+         if(extent == 'full')
+            lines(session$userData$footprint, lwd = 3, col = 'red')
+         dev.off()
+         cat('Making image takes ', Sys.time() - a, ' secs\n', sep = '')
+      
+      a <- Sys.time()
+      z <- renderImage({
+         list(src = cache,
+              contentType = 'image/png',
+              width = 800,
+              height = 800)
+      }, 
+      deleteFile = FALSE)
+   }
+   cat('Rendering image takes ', Sys.time() - a, ' secs\n', sep = '')
+   z
 }
