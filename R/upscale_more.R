d99270e26fa921d04c9677bbf6871db8417d33a4
bwcompton <bcompton@umass.edu>
2025-11-21 22:44:49 +0000

    Debug updscale_more

--- R/upscale_more.R
+++ R/upscale_more.R
@@ -22,16 +22,21 @@ upscale_more <- function(site, newsite, cellsize, metrics = 'all', cache = TRUE)
    
    
    # metrics sd, median, skewness, and kurtosis are already defined. Here are the rest:
-   q05 <- function(x) quantile(x, 0.05)
-   q10 <- function(x) quantile(x, 0.10)
-   q25 <- function(x) quantile(x, 0.25)
-   q75 <- function(x) quantile(x, 0.75)
-   q90 <- function(x) quantile(x, 0.90)
-   q95 <- function(x) quantile(x, 0.95)
+   sd <- function(x) stats::sd(x, na.rm = TRUE)
    
-   r0595 <- function(x) diff(quantile(x, c(0.05, 0.95)), 1)
-   r1090 <- function(x) diff(quantile(x, c(0.05, 0.95)), 1)
-   iqr <- function(x) IQR(x)
+   q05 <- function(x) quantile(x, 0.05, na.rm = TRUE)
+   q10 <- function(x) quantile(x, 0.10, na.rm = TRUE)
+   q25 <- function(x) quantile(x, 0.25, na.rm = TRUE)
+   q75 <- function(x) quantile(x, 0.75, na.rm = TRUE)
+   q90 <- function(x) quantile(x, 0.90, na.rm = TRUE)
+   q95 <- function(x) quantile(x, 0.95, na.rm = TRUE)
+   
+   r0595 <- function(x) diff(quantile(x, c(0.05, 0.95), na.rm = TRUE), 1)
+   r1090 <- function(x) diff(quantile(x, c(0.05, 0.95), na.rm = TRUE), 1)
+   iqr <- function(x) IQR(x, na.rm = TRUE)
+   
+   skewness <- function(x) moments::skewness(x, na.rm = TRUE)
+   kurtosis <- function(x) moments::kurtosis(x, na.rm = TRUE)
    
    
    if(length(metrics) == 1 & metrics[1] == 'all')
@@ -44,12 +49,12 @@ upscale_more <- function(site, newsite, cellsize, metrics = 'all', cache = TRUE)
    db_name <- paste0('flights_', site, '.txt')                                      # copy flights database and nuke stuff we don't want to keep
    db <- read.table(file.path(source, db_name), ,
                     sep = '\t', quote = '', header = TRUE)
- 
+   
    db <- db[!db$deleted & nchar(db$window) == 0 & db$score != 1, ]                  # we're keeping everything but deleted, upscaled and rejected orthos
    orthos <- db$name                                                                # target orthos
    
    
-   x <- rast(file.path(result, orthos[1]))                                          # get rescaling factor from (previously-processed) first raster
+   x <- rast(file.path(source, orthos[1]))                                          # get rescaling factor from the first raster
    x <- project(x, 'epsg:26986')
    factor <- round(cellsize / res(x)[1])                                            # integer rescaling factor
    message('Rescaling factor = ', factor)
@@ -62,8 +67,10 @@ upscale_more <- function(site, newsite, cellsize, metrics = 'all', cache = TRUE)
       type <- datatype(x, bylyr = FALSE)                                            #    get datatype and missing
       missing <- get_NAflag(x) 
       
+      
       for(j in seq_along(metrics)) {                                                #    for each metric
-         x <- terra::aggregate(x, factor, fun = fns[j])                             #       use aggregate to upscale
+         print(metrics[j])
+         x <- terra::aggregate(x, factor, fun = eval(parse(text = metrics[j])))     #       use aggregate to upscale
          name <- paste0(file_path_sans_ext(orthos[i]), '__', metrics[j], '.tif')    #       result file name
          writeRaster(x, file.path(result, name), 
                      overwrite = TRUE, datatype = type, NAflag = missing)           #       save raster
