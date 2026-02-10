62a898874af19abdc073d699217b10ea7129a698
Bradley W. Compton <bcompton@umass.edu>
2026-02-10 13:36:05 -0500

    Drop message about terra version, as a newer version is out with my change; update README

--- inst/scripts/unet_test_fit.R
+++ inst/scripts/unet_test_fit.R
@@ -6,11 +6,20 @@ source_python("inst/python/train_unet.py")
 
 
 original_classes <- c(3, 4, 5, 6)  # Maps to 0, 1, 2, 3 internally
+site <- 'nor'
+model <- 'unet02'
+data_dir <- resolve_dir('X:/projects/uas/marshmap/data/<site>/unet', site) 
+output_dir <- file.path(data_dir, 'models')
+
 
 # Call the training function
 result <- train_unet(
-   data_dir = "X:/projects/uas/marshmap/data/rr/unet/unet01",
-   site = "rr",
+ #  data_dir = "X:/projects/uas/marshmap/data/rr/unet/unet01",
+ #  data_dir = "X:/projects/uas/marshmap/data/nor/unet/unet02",
+
+   site = "nor",
+   data_dir = data_dir,
+   output_dir = output_dir,
    n_epochs = 50L,
    batch_size = 8L,
    learning_rate = 0.0001,
